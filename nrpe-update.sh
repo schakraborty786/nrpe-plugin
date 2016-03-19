20e1d42e773853af3506d139027bac57e6326371
nik786 <schakraborty@HealthExpense.com>
2016-03-19 13:06:00 +0530

    changes done

--- nrpe-update.sh
+++ nrpe-update.sh
@@ -2,9 +2,15 @@
 
 k=`date +%Y%m%d`
 
-mv /opt/hex/nagios/plugins/  /opt/he/nrpe-back/plugins-$k
+#mv /opt/hex/nagios/plugins/  /opt/he/nrpe-back/plugins-$k
 
-mv /opt/he/libexec        /opt/hex/nagios/plugins 
+#mv /opt/he/libexec        /opt/hex/nagios/plugins 
+
+cp -rvf /opt/he/libexec/check_file /opt/hex/nagios/plugins
+
+cp -rvf /opt/he/libexec/check_sftp_file /opt/hex/nagios/plugins
+
+#cp /opt/he/libexec/nrpe.cfg  /etc/nrpe.cfg
 
 #/etc/init.d/nagios-nrpe-server   restart
 
